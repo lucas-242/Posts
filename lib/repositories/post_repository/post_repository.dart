5b6b0a25fbc2af4b71ef058dcae7ebe4b624f774
Lucas Guimarães <guimaraeslucas242@gmail.com>
2023-06-09 12:07:18 -0300

    feature: implemented post repository

--- lib/repositories/post_repository/post_repository.dart
+++ lib/repositories/post_repository/post_repository.dart
@@ -2,6 +2,7 @@ import 'package:reddit_posts/core/models/post.dart';
 
 abstract interface class PostRepository {
   Future<List<Post>> get();
+  Future<List<Post>> getLocal();
   Future<Post> create(Post post);
   Future<void> update(Post post);
   Future<void> delete(int id);
--- lib/repositories/post_repository/reddit/reddit_post_repository.dart
+++ lib/repositories/post_repository/reddit/reddit_post_repository.dart
@@ -1,12 +1,16 @@
+import 'dart:math';
+
 import 'package:reddit_posts/core/connections/connection.dart';
 import 'package:reddit_posts/core/models/post.dart';
+import 'package:reddit_posts/data/post_database/post_database.dart';
 import 'package:reddit_posts/repositories/post_repository/post_repository.dart';
 import 'package:reddit_posts/repositories/post_repository/reddit/models/reddit_response.dart';
 
 final class RedditPostRepository implements PostRepository {
-  RedditPostRepository(this._connection);
+  RedditPostRepository(this._connection, this._database);
   final Connection _connection;
   final String _apiUrl = 'https://api.reddit.com/r/artificial/hot';
+  final PostDatabase _database;
 
   @override
   Future<List<Post>> get() async {
@@ -15,20 +19,20 @@ final class RedditPostRepository implements PostRepository {
   }
 
   @override
-  Future<Post> create(Post post) {
-    // TODO: implement create
-    throw UnimplementedError();
+  Future<List<Post>> getLocal() async => _database.get();
+
+  @override
+  Future<Post> create(Post post) async {
+    final posts = await getLocal();
+    final lastId = posts.isEmpty ? -1 : posts.map((e) => e.id).fold(0, max);
+    final toCreate = post.copyWith(id: lastId);
+    await _database.create(toCreate);
+    return toCreate;
   }
 
   @override
-  Future<void> delete(int id) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
+  Future<void> delete(int id) => _database.delete(id);
 
   @override
-  Future<void> update(Post post) {
-    // TODO: implement update
-    throw UnimplementedError();
-  }
+  Future<void> update(Post post) async => _database.update(post);
 }
